ad2c4671c82cd9c1cefbcd5158cb568ba88221e4
ksn199ms <mubashirpannur123@gmail.com>
2025-12-01 20:19:32 +0530

    inngest-setup is done and vercel deploying configuration added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
 import { clerkMiddleware } from '@clerk/express';
 
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send('Server is live!'));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000
 
